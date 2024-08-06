844cdd354cbb77245c0a3da0b8ce1fda0f1043eb
Marko <markogidej@yahoo.com>
2024-08-06 12:13:53 -0400

    Init Commit

---
+++ Implementing a Doubly Linked List in Python.py
@@ -0,0 +1,10 @@
+# Task 1 Implement the **Node** class to represent individual nodes in the doubly linked list.
+
+
+
+# Task 2 Implement the **DoublyLinkedList** class with methods for insertion, deletion, and traversal.
+
+
+
+# Task 3 Test the implemented functionality by performing various operations on the doubly linked list.
+
---
+++ Implementing a Singly Linked List in Python.py
@@ -0,0 +1,10 @@
+# Task 1 Implement the Node class to represent individual nodes in the linked list.
+
+
+
+# Task 2 Implement the SinglyLinkedList class with methods for insertion, deletion, and traversal.
+
+
+
+# Task 3 Test the implemented functionality by performing various operations on the linked list.
+
